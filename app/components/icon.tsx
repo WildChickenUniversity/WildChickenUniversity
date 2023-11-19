7a1919406bfd059c828769a2430c86ac9881e3ad
Mr-Sheep <24599209+Mr-Sheep@users.noreply.github.com>
2023-11-18 21:12:53 -0600

    darkmode styling

--- app/components/icon.tsx
+++
@@ -1,5 +0,0 @@
-const iconSwitch = (theme: any) => {
-  return <a className="ml-1 mr-1">{theme === "dark" ? "🌞" : "🌕"}</a>;
-};
-
-export default iconSwitch;
--- app/components/theme-switch.tsx
+++ app/components/theme-switch.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import { useTheme } from "next-themes";
-import iconSwitch from "./icon";
 
 const Button = () => {
   const { systemTheme, theme, setTheme } = useTheme();
@@ -10,9 +9,9 @@ const Button = () => {
   return (
     <button
       onClick={() => (theme == "dark" ? setTheme("light") : setTheme("dark"))}
-      className="bg-gray-800 dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 text-white dark:text-gray-800 text-sm md:text-sm rounded-lg  "
+      className="px-1 bg-gray-800 dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 text-white dark:text-gray-800 text-sm md:text-sm rounded-lg  "
     >
-      {iconSwitch(theme)}
+      {theme === "dark" ? "🌞" : "🌕"}
     </button>
   );
 };
