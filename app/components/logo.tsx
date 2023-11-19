126f306771bf447eede84496eede37d3e8775f61
Mr-Sheep <24599209+Mr-Sheep@users.noreply.github.com>
2023-11-18 22:42:26 -0600

    fixing dark theme

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -1,29 +1,25 @@
+"use client";
 import Image from "next/image";
 import TWrapper from "./typewriter";
 import React from "react";
-import { useTheme } from "next-themes";
 
 const Chicken: React.FC = () => {
-  const { theme } = useTheme();
-  const effectiveTheme = theme ?? "light";
-  // const [clientTheme, setClientTheme] = useState(theme);
-
-  // useEffect(() => {
-  //   setClientTheme(theme);
-  // }, [theme]);
-
   return (
     <div className="relative flex flex-col place-items-center before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[200px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[150px]">
       <Image
-        className="relative "
-        src={
-          effectiveTheme === "dark"
-            ? "/Wild_Chicken_White.svg"
-            : "/Wild_Chicken.svg"
-        }
+        className="relative block dark:hidden"
+        src="/Wild_Chicken.svg"
         alt="WCU Logo"
-        width={180}
-        height={37}
+        width={270}
+        height={55.5}
+        priority
+      />
+      <Image
+        className="relative hidden dark:block"
+        src="/Wild_Chicken_White.svg"
+        alt="WCU Logo"
+        width={270}
+        height={55.5}
         priority
       />
       <div className="dark:text-gray-300 font-semibold text-center text-sm mt-2">
--- app/components/theme-switch.tsx
+++ app/components/theme-switch.tsx
@@ -1,18 +1,29 @@
 "use client";
 import React from "react";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 const Button = () => {
   const { systemTheme, theme, setTheme } = useTheme();
   const currentTheme = theme === "system" ? systemTheme : theme;
 
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
-    <button
-      onClick={() => (theme == "dark" ? setTheme("light") : setTheme("dark"))}
-      className="px-1 bg-gray-800 dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 text-white dark:text-gray-800 text-sm md:text-sm rounded-lg  "
-    >
-      {theme === "dark" ? "🌞" : "🌕"}
-    </button>
+    isClient && (
+      <button
+        onClick={() =>
+          currentTheme == "dark" ? setTheme("light") : setTheme("dark")
+        }
+        className="px-1 bg-gray-800 dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 text-white dark:text-gray-800 text-sm md:text-sm rounded-lg  "
+      >
+        {theme === "dark" ? "🌞" : "🌕"}
+      </button>
+    )
   );
 };
 
