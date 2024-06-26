00d99f28a662989cc24b6a9e91ede32b98cd747e
Mr-Sheep <24599209+Mr-Sheep@users.noreply.github.com>
2024-06-26 13:22:32 -0500

    renaming file

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/footer";
-import Provider from "./components/Provider";
+import Provider from "./components/provider";
 import Comment from "./components/comment";
 
 const inter = Inter({ subsets: ["latin"] });
