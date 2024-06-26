c3127ed6b705e2233bf00e7e3130de275eddf42b
Mr-Sheep <24599209+Mr-Sheep@users.noreply.github.com>
2024-06-26 13:22:53 -0500

    renaming file

--- app/components/Provider.tsx
+++ app/components/Provider.tsx
@@ -2,6 +2,6 @@
 
 import { ThemeProvider } from "next-themes";
 
-export default function Providers({ children }: any) {
+export default function Provider({ children }: any) {
   return <ThemeProvider attribute="class">{children}</ThemeProvider>;
 }
