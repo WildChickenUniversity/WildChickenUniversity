48841a8bface8c588dcbc1ddc186b8c80aaffda7
Mr-Sheep <24599209+Mr-Sheep@users.noreply.github.com>
2025-09-13 04:20:54 -0500

    fixing github action

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,13 @@
-
-
 import createMDX from "@next/mdx";
+import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   experimental: {
     mdxRs: true, // Use Rust-based MDX compiler for faster builds
   },
+  output: "export",
 };
 
 const withMDX = createMDX({
