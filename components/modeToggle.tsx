17727af41572398e9e7b551b7d0842e3afc0712e
Mr-Sheep <24599209+Mr-Sheep@users.noreply.github.com>
2025-09-19 11:56:41 -0500

    cleanup

--- components/modeToggle.tsx
+++ components/modeToggle.tsx
@@ -18,7 +18,7 @@ export default function ModeToggle() {
         <Button variant="ghost" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
+          <span className="sr-only">Toggle Theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
