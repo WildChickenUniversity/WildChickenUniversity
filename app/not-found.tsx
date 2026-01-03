471dd58502f31d21555998a93f26d346ac5ada8e
Mr-Sheep <24599209+Mr-Sheep@users.noreply.github.com>
2026-01-02 23:14:30 -0600

    adding customized 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black">
+      <h1 className="text-white font-bold text-3xl">418 - I'm a teapot</h1>
+      <p>Unfortunately we cannot custimize the http status code to 418, but please enjoy this lovely cat picture :&#41;</p>
+      <Image
+        src="https://http.cat/418"
+        alt="418"
+        width={750}
+        height={600}
+        priority
+      />
+      <Button
+        className="w-30 h-15 text-xl font-semibold"
+        asChild
+      >
+        <Link href="/">Go Back</Link>
+      </Button>
+    </div>
+  );
+}
